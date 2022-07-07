cacb6707c1e346b14f74ff4d4868fbe49e235bf5
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-07 20:17:07 -0300

    Requisitos 5, 6 ,7 e 8 :D

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ header.appendChild(headerTitle);
 headerTitle.innerText = 'Minha Lista de Tarefas';
 
 const funcionamento = document.createElement('h2');
-funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+funcionamento.innerText =
+  'Clique duas vezes em um item para marcá-lo como completo';
 funcionamento.id = 'funcionamento';
 header.appendChild(funcionamento);
 
@@ -16,6 +17,36 @@ main.appendChild(inputTarefa);
 inputTarefa.type = 'text';
 inputTarefa.id = 'texto-tarefa';
 
+const buttonCriarTarefa = document.createElement('button');
+buttonCriarTarefa.id = 'criar-tarefa';
+buttonCriarTarefa.innerText = 'Criar Tarefa';
+main.appendChild(buttonCriarTarefa);
+
 const oList = document.createElement('ol');
 oList.id = 'lista-tarefas';
 main.appendChild(oList);
+
+function criarTarefa(event) {
+  if (event.target.id === 'criar-tarefa') {
+    const tarefaTexto = inputTarefa.value;
+    console.log(tarefaTexto);
+    inputTarefa.value = '';
+    const novaTarefa = document.createElement('li');
+    novaTarefa.innerText = tarefaTexto;
+    novaTarefa.className = 'tarefa';
+    oList.appendChild(novaTarefa);
+  }
+}
+document.addEventListener('click', criarTarefa);
+
+function tarefasClicadas(event) {
+  if (event.target.classList.contains('tarefa')) {
+    const tarefas = document.querySelectorAll('.tarefa');
+    for (let i = 0; i < tarefas.length; i += 1) {
+      tarefas[i].classList.remove('tarefa-clicada');
+    }
+    event.target.classList.add('tarefa-clicada');
+  }
+}
+
+document.addEventListener('click', tarefasClicadas);
