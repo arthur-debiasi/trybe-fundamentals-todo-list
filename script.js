007435b3a3cc7e40794921a08a80dda276bf9af2
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-07 10:11:46 -0300

    Requisito 1 - header e título

---
+++ script.js
@@ -0,0 +1,7 @@
+const main = document.createElement('main');
+document.body.appendChild(main);
+const header = document.createElement('header');
+main.appendChild(header);
+const headerTitle = document.createElement('h1');
+header.appendChild(headerTitle);
+headerTitle.innerText = 'Minha Lista de Tarefas';
